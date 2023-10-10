1fd2e4b67936ac55ed2863cddcbc9d3668461440
Ilya <ilieturcan424@gmail.com>
2023-10-10 17:48:21 +0300

    final commit

--- Lab2.py
+++ Lab2.py
@@ -2,6 +2,19 @@
 import csv
 from bs4 import BeautifulSoup
 
+# # pip install requests-html
+# from requests_html import HTMLSession
+
+# session = HTMLSession()
+# rs = session.get('https://www.gismeteo.ru/diary/4980/2023/9/')
+# with open('rs_before_js.html', 'w', encoding='utf-8') as f:
+#     f.write(rs.html.html)
+
+# rs.html.render()  # Без этого не будет выполнения js кода
+
+# with open('rs_after_js.html', 'w', encoding='utf-8') as f:
+#     f.write(rs.html.html)
+
 with open("index.html", encoding="utf8") as file:
     src = file.read()
 
